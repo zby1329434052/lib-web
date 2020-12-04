4156087636d886facd3b4b90913df94e5631c156
zby1329434052 <1329434052@qq.com>
2020-12-05 01:12:50 +0800

    feat dev分支创建

---
+++ lib-web/src/main/java/com/zhang/libweb/LibWebApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.libweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibWebApplication.class, args);
+    }
+
+}
---
+++ lib-web/src/test/java/com/zhang/libweb/LibWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhang.libweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
